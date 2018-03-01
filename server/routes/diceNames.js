3f0f5a4cf04840c8059c02bbc5a38db0e858005b
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-03-02 08:19:42 +1300

    Set up api route to get diceNames

--- server/routes/diceNames.js
+++ server/routes/diceNames.js
@@ -3,5 +3,18 @@ const { decode } = require('../auth/token')
 const express = require('express')
 const router = express.Router()
 
+const db = require('../db/dice')
+
+// get all the dice details from the dice_name table and send back to client
+router.get('/', decode, (req, res) => {
+  db.getDiceNames()
+    .then(dice => {
+      res.json(dice)
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE_ERROR: ' + err.message)
+    })
+})
+
 
 module.exports = router
