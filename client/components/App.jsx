90e2cc3f250b7f36513ad38839f595c35077e166
edi-rose <edirose1998@gmail.com>
2018-03-01 15:33:09 +1300

    merges soleved

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import {HashRouter as Router, Route, Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 
-import Login from './Login'
 import Register from './Register'
 import Nav from './Nav'
 import Meeting from './Meeting'
 import History from './History'
+import Home from './Home'
 
 const App = ({auth}) => (
   <Router>
@@ -22,10 +22,7 @@ const App = ({auth}) => (
       </div>
 
       <div className=''>
-        {!auth.isAuthenticated &&
-          <Route exact path="/" component={Login} />
-        }
-        <Route path="/login" component={Login} />
+        <Route path="/" component={Home}/>
         <Route path="/register" component={Register} />
         <Route path="/meeting" component={Meeting} />
         <Route path="/history" component={History} />
