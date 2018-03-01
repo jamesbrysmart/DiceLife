818703ebc1db32fbb3c49a0dfba61e0c715c8e2f
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-03-02 08:23:28 +1300

    Added in query to getDiceNames

--- server/db/dice.js
+++ server/db/dice.js
@@ -5,10 +5,17 @@ const connection = require('knex')(config)
 function getDiceOptions (testDb) {
   const db = testDb || connection
   return db('dice_names')
-  .join('dice_options', 'dice_names.id', 'dice_options.dice_names_id') 
-  }  
+  .join('dice_options', 'dice_names.id', 'dice_options.dice_names_id')
+  }
+
+function getDiceNames (testDb) {
+  const db = testDb || connection
+  return db('dice_names')
+    .select()
+}
 
 module.exports = {
   getDiceOptions,
+  getDiceNames,
   connection
-} 
+}
