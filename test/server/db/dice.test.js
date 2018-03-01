c3cc0a7651e346a4f89b31e75ccba1dfe7f2f79a
kahu-2018 <fake@fake.com>
2018-03-01 18:42:16 +1300

    tests

--- test/server/db/dice.test.js
+++ test/server/db/dice.test.js
@@ -15,5 +15,13 @@ test('read dice db', () => {
   return diceDb.getDiceOptions(testDb)
     .then(dice => {
       expect(dice.length).toBe(15)
+      expect(dice[0]['id']).toEqual(2)
+    })
+})
+
+test('dice name and options included - join table works', () => {
+  return diceDb.getDiceOptions(testDb)
+    .then(dice => {
+      expect(Object.keys(dice[0]).length).toEqual(6)
     })
 })
