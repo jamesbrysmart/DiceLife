4da843264677566dbe80531d9b2aff34f4221a95
kahu-2018 <fake@fake.com>
2018-03-01 20:57:28 +1300

    more actions, some tests, test suite not working though

--- client/actions/diceOutcomes.js
+++ client/actions/diceOutcomes.js
@@ -13,6 +13,21 @@ function receiveDiceOutcomes(outcomes) {
   }
 }
 
+function addDiceOutcome(outcome) {
+  return {
+    type:'ADD_DICE_OUTCOME',
+    outcome
+  }
+}
+
+function editDiceOutcome(oldDice, newDice) {
+  return {
+    type:'EDIT_DICE_OUTCOME',
+    oldDice,
+    newDice
+  }
+}
+
 function showError(errorMessage) {
   return {
     type:'SHOW_ERROR',
@@ -30,5 +45,22 @@ export function getDiceOutcomes () {
     .catch(err => {
       dispatch(showError(err.message))
     })
+  }
+}
+
+export function addNewDice (dice) {
+  return (dispatch) => {
+    request('post', 'diceOptions', dice)
+      .then(res => dispatch(addDiceOutcome(res.body)))
+      .catch(err => console.log({err}))
+  }
+}
+
+export function editDice (oldDice, newDice){
+  return (dispatch)=> {
+    request('put', 'diceOptions', oldDice.id)
+      .send(newDice)
+      .then(res=>dispatch(editDiceOutcome(res.body)))
+      .catch(err=> console.log({err}))
   }
 }
