47ec5100bae52625f057538fba3487a66825344e
kahu-2018 <fake@fake.com>
2018-03-02 12:23:34 +1300

    Fixed getDiceOutcome changes

--- client/reducers/diceOutcomes.js
+++ client/reducers/diceOutcomes.js
@@ -4,7 +4,7 @@ function diceOutcomes(state=[], action) {
   console.log({action})
   switch(action.type) {
     case 'RECEIVE_DICE_OUTCOME':
-      return [...action.outcomes]
+      return action.outcomes
     case 'ADD_DICE_OUTCOME':
       return [...state, action.outcome]
     // case 'EDIT_DICE_OUTCOME':
