af74b6dc7bf822f27f8142fa86fb59ca0742dc83
kahu-2018 <fake@fake.com>
2018-03-02 14:59:00 +1300

    Updated page header to redux

--- client/components/SpecificDice.jsx
+++ client/components/SpecificDice.jsx
@@ -1,20 +1,47 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import RollDiceButton from './RollDiceButton'
+
 
 class SpecificDice extends React.Component {
   constructor(props) {
     super(props)
-
+    this.state = {
+      diceOne: 0,
+      diceTwo: 0,
+      diceTotal: 0
+    }
+    this.rollTheDice = this.rollTheDice.bind(this)
+    this.diceRoll =this.diceRoll.bind(this)
   }
+
+  diceRoll() {
+    return Math.floor(Math.random() * 6) + 1
+  }
+
+  rollTheDice(){
+    var d1 = this.diceRoll()
+    var d2 = this.diceRoll()
+    var total = d1 + d2
+
+    this.setState({
+     diceOne:d1,
+     diceTwo: d2,
+     diceTotal: total
+    })
+  }
+
   componentDidMount() {
 
   }
   render() {
     const diceOptions = this.props.diceOutcomes.dice
-    console.log(diceOptions)
+    const diceId = this.props.match.params.id
+    const specificDice = this.props.diceOutcomes.dice[diceId].dice_name
+    console.log(specificDice)
     return (
       <div>
-        <h2></h2>
+        <h2>{specificDice}</h2>
         <table>
           <tbody>
             {diceOptions.map((dice, i) => {
@@ -22,6 +49,7 @@ class SpecificDice extends React.Component {
             })}
           </tbody>
         </table>
+        <RollDiceButton rollTheDice= {this.rollTheDice}/>
       </div>
     )
   }
