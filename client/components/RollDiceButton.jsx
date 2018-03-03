508b450fe83a3c92d7d5ecbd69eace36f100454d
edi-rose <edirose1998@gmail.com>
2018-03-03 18:25:16 +1300

    Got Animation Linking To ResultParent

--- client/components/RollDiceButton.jsx
+++ client/components/RollDiceButton.jsx
@@ -5,7 +5,7 @@ export default function RollDiceButton (props) {
   return (
     <div className="container">
     <div className='button is-danger'>
-      <Link to='/resultsparent' onClick ={props.rollTheDice}>Roll The Dice</Link>
+      <Link to='/animation' onClick ={props.rollTheDice}>Roll The Dice</Link>
     </div>
     <div className='button is-danger'>
       <Link to='/allDice'> Back </Link>
