d1bd2d162b80d6646328ccebcbc92e50d40005b3
kahu-2018 <fake@fake.com>
2018-03-03 18:38:42 +1300

    specificdicetableupdated

--- client/components/SpecificDice.jsx
+++ client/components/SpecificDice.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import RollDiceButton from './RollDiceButton'
 import diceRolls from '../actions/diceRolls'
 import diceID from '../actions/diceID'
@@ -9,7 +9,7 @@ class SpecificDice extends React.Component {
     super(props)
 
     this.rollTheDice = this.rollTheDice.bind(this)
-    this.diceRoll =this.diceRoll.bind(this)
+    this.diceRoll = this.diceRoll.bind(this)
   }
 
 
@@ -17,7 +17,7 @@ class SpecificDice extends React.Component {
     return Math.floor(Math.random() * 6) + 1
   }
 
-  rollTheDice(){
+  rollTheDice() {
     var d1 = this.diceRoll()
     var d2 = this.diceRoll()
     var total = d1 + d2
@@ -30,13 +30,13 @@ class SpecificDice extends React.Component {
     this.props.dispatch(diceID(this.props.match.params.id))
   }
   render() {
-// getting the name of the selected dice to filter options
+    // getting the name of the selected dice to filter options
     const diceOutcomes = this.props.diceOutcomes.dice
     const diceId = this.props.match.params.id
 
-    const diceNames = this.props.diceNames[diceId-1]['dice_name']
+    const diceNames = this.props.diceNames[diceId - 1]['dice_name']
 
-// Filter the diceOutcomes by the specific dice name
+    // Filter the diceOutcomes by the specific dice name
     const specificDiceOptions = diceOutcomes.filter((dice, i) => {
       if (dice.dice_name == diceNames) {
         return dice
@@ -46,20 +46,29 @@ class SpecificDice extends React.Component {
       <div>
         <h2 className="title is-4">{diceNames}</h2>
         <div className="columns">
-        <span className="column is-3"></span>
-        <span className="column is-6">
-        <table className="table is-fullwidth" id="bgimg">
+          <span className="column is-3"></span>
+          <span className="column is-6">
+            <table className="table is-fullwidth" id="bgimg">
 
-          <tbody>
-            {specificDiceOptions.map((dice, i) => {
-              return <tr key={i}><td><p>{dice.dice_option}</p></td></tr>
-            })}
-          </tbody>
-        </table>
-        </span>
-        <span className="column is-3"></span>
+              <tbody id="makewhite">
+                {/* this is for the headers of the table */}
+                <tr>
+                  <th>Number</th>
+                  <th>Chance</th>
+                  <th>What To Do:</th>
+                </tr>
+
+                <tr><td>1</td><td>likely</td><td><p>Roll Again</p></td></tr>
+                {specificDiceOptions.map((dice, i) => {
+                  return <tr key={i}><td>1</td><td>likely</td><td><p>{dice.dice_option}</p></td>
+                  </tr>
+                })}
+              </tbody>
+            </table>
+          </span>
+          <span className="column is-3"></span>
         </div>
-        <RollDiceButton rollTheDice= {this.rollTheDice}/>
+        <RollDiceButton rollTheDice={this.rollTheDice} />
       </div>
     )
   }
