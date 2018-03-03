3cb39a051343fce79349b1ac5de7b6e3f8afbec3
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-03-03 16:19:50 +1300

    Set up addNewDiceOptions to insert multiple options for a dice

--- server/db/dice.js
+++ server/db/dice.js
@@ -23,7 +23,12 @@ function addNewDice(data, testDb) {
 function addNewDiceOptions(result, data, testDb) {
   const db = testDb || connection
   return db('dice_options')
-    .insert({'dice_option': data.body.dice_option,'position': data.body.position, 'dice_names_id': result[0]})
+    .insert([{'dice_option': data.body.dice_option_1,'position': data.body.position_1, 'dice_names_id': result[0]},
+            {'dice_option': data.body.dice_option_2,'position': data.body.position_2, 'dice_names_id': result[0]},
+            {'dice_option': data.body.dice_option_3,'position': data.body.position_3, 'dice_names_id': result[0]},
+            {'dice_option': data.body.dice_option_4,'position': data.body.position_4, 'dice_names_id': result[0]},
+            {'dice_option': data.body.dice_option_5,'position': data.body.position_5, 'dice_names_id': result[0]},
+            {'dice_option': data.body.dice_option_6,'position': data.body.position_6, 'dice_names_id': result[0]}])
 }
 
 module.exports = {
