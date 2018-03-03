d5612c314f2b66e04dbda62e6656db6bdae388da
James Bryant <JB@James-Bryants-MacBook-Pro.local>
2018-03-03 21:21:14 +1300

    addDice talking to database

--- client/components/AddDice.jsx
+++ client/components/AddDice.jsx
@@ -1,13 +1,46 @@
 import React from 'react'
+import {connect} from 'react-redux'
+
+import { addNewDice } from './../actions/diceOutcomes'
 
 class AddDice extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      dice = {
+        diceName: '',
+        user_id: 1,
+        dice_option_1: '',
+        dice_option_2: '',
+        dice_option_3: '',
+        dice_option_4: '',
+        dice_option_5: '',
+        dice_option_6: ''
+      }
+  }
+}  
+  handleChange(e) {
+    this.setState([e.target.name]: e.target.value}) 
+  }
+
+
   render () {
     return (
       <div>
-        hello
+        <form onSubmit={this.props.dispatch(addNewDice(this.state.dice))}>
+          <input type='text' name='dice_name'/>
+          <label for='dice_options'>What to do:</label>
+          <input type='text' name='dice_option_1' onChange={(e) => this.handleChange(e)} />
+          <input type='text' name='dice_option_2' onChange={(e) => this.handleChange(e)} />
+          <input type='text' name='dice_option_3' onChange={(e) => this.handleChange(e)} />
+          <input type='text' name='dice_option_4' onChange={(e) => this.handleChange(e)} />
+          <input type='text' name='dice_option_5' onChange={(e) => this.handleChange(e)} />
+          <input type='text' name='dice_option_6' onChange={(e) => this.handleChange(e)} />
+          <input type='submit'/>
+        </form>
       </div> 
     )
   }
 }
 
-export default AddDice
+export default connect()(AddDice)
