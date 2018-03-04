1b5d04a58c36cfedf2bd9ac74f8d5c6f63fe6402
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-03-04 20:08:49 +1300

    Set up users function to store uid as variable

--- server/db/users.js
+++ server/db/users.js
@@ -4,9 +4,13 @@ function createUser (user_name, first_name, last_name, password, db) {
   return new Promise ((resolve, reject) => {
     hash.generate(password, (err, hash) => {
       if (err) reject(err)
+      const uid = undefined
       db('users')
         .insert({user_name, first_name, last_name, hash})
-        .then(user_id => resolve(user_id))
+        .then((user_id) =>{
+          const uid = user_id
+          console.log(uid)
+          resolve(user_id)})
         .catch(err => reject(err))
     })
   })
@@ -23,10 +27,17 @@ function getUserByName (user_name, db) {
     .first()
 }
 
-
+function setUpUsersDefaultDice(uid) {
+  console.log(`I am running the SetUsersDefaultTable`)
+  return db('dice_names')
+    .insert([{dice_name: 'Friday Night', user_id: uid},
+            {dice_name: 'Weekend Plans', user_id: uid},
+            {dice_name: 'Dinner Plans', user_id: uid}])
+}
 
 module.exports = {
   createUser,
   userExists,
-  getUserByName
+  getUserByName,
+  setUpUsersDefaultDice
 }
