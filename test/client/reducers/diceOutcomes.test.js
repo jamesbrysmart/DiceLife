a3c229d735800769f52285f0ac2adc4c215e1999
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-03-05 18:33:44 +1300

    Set up diceOutcomes reducer tests

--- test/client/reducers/diceOutcomes.test.js
+++ test/client/reducers/diceOutcomes.test.js
@@ -1,5 +1,9 @@
 import reducer from '../../../client/reducers/diceOutcomes'
 
+test('diceOutcomes inital state', () => {
+  expect(reducer(undefined, {})).toEqual([])
+})
+
 test('RECEIVE_DICE_OUTCOME', () => {
   const outcomes = [{id: 1}, {id: 2}]
   const actual = reducer([], {
@@ -7,4 +11,17 @@ test('RECEIVE_DICE_OUTCOME', () => {
     outcomes
   })
   expect(actual).toEqual(outcomes)
+})
+
+
+test('ADD_DICE_OUTCOME', () => {
+  const outcome = {id: 1}
+  const actual = reducer(
+    {},
+    {
+      type: 'ADD_DICE_OUTCOME',
+      outcome
+    }
+  )
+  expect(actual).toEqual([outcome])
 })
