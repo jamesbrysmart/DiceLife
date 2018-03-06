e1cf7d42a70c2d82568e360a7bb5a07c4f9da2bc
edi-rose <edirose1998@gmail.com>
2018-03-06 14:12:10 +1300

    bug fix, use this version

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -29,7 +29,7 @@ class Nav extends React.Component {
         <div id="navbarMenuHeroA" className={`navbar-menu ${showBurger ? "is-active" : ''}`}>
           <div className="navbar-end">
             {auth.isAuthenticated
-              ? [
+              ? 
                 <div>
                   <div className = 'nav1'> 
                     <a className="navbar-item" onClick={() => logout()}>Logout</a>
@@ -38,10 +38,9 @@ class Nav extends React.Component {
                     <Link className="navbar-item" to= '/howto'> How To Play? </Link>
                   </div>
                 </div>
-              ]
-              : [
-               
-              ]
+              : 
+               'hello'
+              
             }
           </div>
         </div>
