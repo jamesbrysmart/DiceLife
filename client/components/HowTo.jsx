66b7a046e12a8a040e10cb3074d8bb13870efde6
HaydenMalan <tatyana.malan@gmail.com>
2018-03-06 22:20:29 +1300

    HowTo is a box, has a header

--- client/components/HowTo.jsx
+++ client/components/HowTo.jsx
@@ -8,12 +8,21 @@ class HowTo extends React.Component {
 
  render(){
      return (
-       <div>
-        
-       <div className= "HowTo">
-        <p> Rule number 1: You must obey the will of the Dice </p>
-       </div>
-       <OKButton toggleHowTo={this.props.toggleHowTo}/>
+       <div className="HowTo">
+        <div className="columns">
+          <span className="column is-3"></span>
+          <div className="column is-6">
+            <h2 className="title is-2" id="makewhite">
+              How To Play:
+            </h2>
+            <div className= "box" id="howtoboxcolor"  >
+       
+              <p> Rule number 1: You must obey the will of the Dice </p>
+  
+              <OKButton toggleHowTo={this.props.toggleHowTo}/>
+            </div>
+          </div>
+        </div>
        </div>
      ) 
   }
