44b1f4770311f252fa1bc5f7d0bf5c8139618d80
Hayley Ha <hami201289@him-mail.ch>
2018-03-07 16:33:49 +1300

    styled timeline

--- client/components/NewHistory.jsx
+++ client/components/NewHistory.jsx
@@ -27,7 +27,7 @@ class NewHistory extends React.Component {
     this.setState({
       userInput: newUserInput
     })
-    
+
   };
 
   render() {
@@ -60,32 +60,68 @@ class NewHistory extends React.Component {
     console.log({ historyRollOptionResultIDs })
 
 
-    
+
 
 
     return (
       <div>
-        <p>Hello World!</p>
-        <div class="timeline">
-          <header class="timeline-header">
-            <span class="tag is-medium is-primary">Start</span>
+        <div className="timeline">
+          <header className="timeline-header">
+            <span className="tag is-medium is-primary">Start</span>
           </header>
-        {arrayOfRolls.map((roll) => {
-          return <div class="timeline-item">
-          <div class="timeline-marker"></div>
-          <div class="timeline-content">
-            <p class="heading">{roll.dice_name}</p>
-            <p>{roll.outcome}</p>
-            <p>{roll.roll_score}</p>
-            <p>{roll.rolled_on}</p>
-            {/* {this.state.userInput.map((input, i) => <p>input</p>)}
-            <input type="text" onChange={this.handleUserInput.bind(this)} placeholder="Did you commit the dice's suggestion?"/> */}
+          <div className="timeline-item">
+            <div className="timeline-marker is-primary"></div>
+            <div className="timeline-content">
+              <p className="heading">January 2016</p>
+              <p>Timeline content - Can include any HTML element</p>
+            </div>
           </div>
-        </div>
-          
-        })}
-            <div class="timeline-header">
-              <span class="tag is-medium is-primary">End</span>
+          <div className="timeline-item">
+            <div className="timeline-marker is-warning is-image is-32x32">
+              <img src="http://bulma.io/images/placeholders/32x32.png"/>
+            </div>
+              <div className="timeline-content">
+                <p className="heading">February 2016</p>
+                <p>Timeline content - Can include any HTML element</p>
+              </div>
+            </div>
+            <header className="timeline-header">
+              <span className="tag is-primary">2017</span>
+            </header>
+            <div className="timeline-item">
+              <div className="timeline-marker is-danger is-icon">
+                <i className="fa fa-flag"></i>
+              </div>
+              <div className="timeline-content">
+                <p className="heading">March 2017</p>
+                <p>Timeline content - Can include any HTML element</p>
+              </div>
+            </div>
+            <header className="timeline-header">
+              <span className="tag is-medium is-primary">End</span>
+            </header>
+          </div>
+          <p>Hello World!</p>
+          <div className="timeline">
+            <header className="timeline-header">
+              <span className="tag is-medium is-primary">Start</span>
+            </header>
+            {arrayOfRolls.map((roll) => {
+              return <div className="timeline-item">
+                <div className="timeline-marker"></div>
+                <div className="timeline-content">
+                  <p className="heading">{roll.dice_name}</p>
+                  <p>{roll.outcome}</p>
+                  <p>{roll.roll_score}</p>
+                  <p>{roll.rolled_on}</p>
+                  {/* {this.state.userInput.map((input, i) => <p>input</p>)}
+            <input type="text" onChange={this.handleUserInput.bind(this)} placeholder="Did you commit the dice's suggestion?"/> */}
+                </div>
+              </div>
+
+            })}
+            <div className="timeline-header">
+              <span className="tag is-medium is-primary">End</span>
             </div>
           </div>
 
@@ -93,20 +129,20 @@ class NewHistory extends React.Component {
         </div>
         )
       }
-    
+
     }
-    
-    
+
+
 const mapStateToProps = (state)=> {
           console.log(state)
   return {
           names: state.diceNames,
-        diceID: state.diceID,
-        outcomes: state.diceOutcomes,
-        userID: state.userID,
-        diceHistory: state.diceHistory,
-        auth: state.auth
-      }
+      diceID: state.diceID,
+      outcomes: state.diceOutcomes,
+      userID: state.userID,
+      diceHistory: state.diceHistory,
+      auth: state.auth
     }
-    
+  }
+  
 export default connect(mapStateToProps)(NewHistory)
