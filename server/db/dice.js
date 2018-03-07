c8174edf6949696ee0649b8ef25276fe1d5690ee
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-03-07 17:30:17 +1300

    updates to queries

--- server/db/dice.js
+++ server/db/dice.js
@@ -17,7 +17,7 @@ function getDiceNames (testDb) {
 function addNewDice(data, testDb) {
   const db = testDb || connection
   return db('dice_names')
-    .insert({dice_name: data.body.dice_name, user_id: data.body.user_id, active: data.body.active})
+    .insert({dice_name: data.body.dice_name, user_id: data.body.user_id, active: data.body.active}, 'id')
 }
 
 function addNewDiceOptions(result, data, testDb) {
@@ -27,7 +27,7 @@ function addNewDiceOptions(result, data, testDb) {
             {'dice_option': data.body.dice_option_2,'position': data.body.position_2, 'dice_names_id': result[0]},
             {'dice_option': data.body.dice_option_3,'position': data.body.position_3, 'dice_names_id': result[0]},
             {'dice_option': data.body.dice_option_4,'position': data.body.position_4, 'dice_names_id': result[0]},
-            {'dice_option': data.body.dice_option_5,'position': data.body.position_5, 'dice_names_id': result[0]}])
+            {'dice_option': data.body.dice_option_5,'position': data.body.position_5, 'dice_names_id': result[0]}], 'id')
 }
 
 function inactiveDice(id, testDb) {
@@ -52,7 +52,7 @@ function getDiceHistory(testDb) {
 function addNewRoll(data, testDb) {
   const db = testDb || connection
   return db('previous_dice_rolls')
-    .insert({roll_score: data.body.roll_score, user_id: data.body.user_id, dice_name_id: data.body.dice_name_id, dice_option_id: data.body.dice_option_id})
+    .insert({roll_score: data.body.roll_score, user_id: data.body.user_id, dice_name_id: data.body.dice_name_id, dice_option_id: data.body.dice_option_id}, 'id')
 }
 
 module.exports = {
