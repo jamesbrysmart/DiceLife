bef51ec619971bbc718a1f9f636a2dc131f87d23
edi-rose <edirose1998@gmail.com>
2018-03-07 18:02:33 +1300

    removed ids from seeds

--- seeds/dice_names.js
+++ seeds/dice_names.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('dice_names').insert([
-        {id: 1, dice_name: 'Friday Night', user_id: null, active: true},  //user_id null means available to all,
-        {id: 2, dice_name: 'Weekend Plans', user_id: null, active: true},
-        {id: 3, dice_name: 'Dinner Plans', user_id: null, active: true }
+        {dice_name: 'Friday Night', user_id: null, active: true},  //user_id null means available to all,
+        {dice_name: 'Weekend Plans', user_id: null, active: true},
+        {dice_name: 'Dinner Plans', user_id: null, active: true }
       ]);
     });
 };
--- seeds/populateUsers.js
+++ seeds/populateUsers.js
@@ -7,7 +7,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, user_name: 'symesharr', first_name: 'Harrison', last_name: 'Symes', hash: hashSync('Krang', saltRounds)}
+        {user_name: 'symesharr', first_name: 'Harrison', last_name: 'Symes', hash: hashSync('Krang', saltRounds)}
       ]);
     });
 };
